fe3744bb33693c35118128aea50725ed9bf51adc
belka2239 <belka2239@gmail.com>
2022-06-03 17:52:20 +0300

    Initial Commit

---
+++ SliderAppSwiftUI/SliderAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderAppSwiftUIApp.swift
+//  SliderAppSwiftUI
+//
+//  Created by VG on 03.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
